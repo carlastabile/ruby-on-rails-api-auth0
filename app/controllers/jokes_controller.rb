d73460f5c1499fa81b217f19dc3fdba58494c5ec
Carla Urrea Stabile <carla.urrea@xing.com>
2022-05-14 21:08:05 +0200

    add jokes controller :index & :create

---
+++ app/controllers/jokes_controller.rb
@@ -0,0 +1,31 @@
+class JokesController < ApplicationController
+  def index
+    render json: Joke.all
+  end
+
+  def create
+    @joke = Joke.new(joke_params)
+    render json: @joke && return if @joke.save
+
+    render json: { error: "Unable to save joke: #{@joke.errors.messages}" }, status: 400
+  end
+
+  private
+
+  # Specify which params are required, this method can be called in any action
+  # but we only use it now for the create method.
+  # we require the body to have the following form:
+  # {
+  #     "joke": {
+  #       "category": "foo",
+  #       "joke": "foobarbaz123"
+  #     }
+  # }
+  #
+  # Note that permit, as the name indicates, allows this parameters to be passed
+  # but it doesn't make them mandatory.
+  # By doing this we are using what's known as string parameters.
+  def joke_params
+    params.require(:joke).permit(:category, :joke)
+  end
+end
