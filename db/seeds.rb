335ce02163cb738248c9bd43a55364c67d8967c1
Carla Urrea Stabile <carla.urrea@xing.com>
2022-05-14 20:52:51 +0200

    Add jokes.json data from Jokes API. add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# The data contained in the jokes.json file comes from the Jokes API https://v2.jokeapi.dev/
+# by running the following GET request: 
+# https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10 
+# This project is just using it to provide some examples
+
+# Read from the JSON file and parse it into a hash
+file = File.read("#{Rails.root.join('jokes.json')}")
+data_hash = JSON.parse(file).deep_symbolize_keys
+
+# Create Jokes into the database
+data_hash[:jokes].each do |joke|
+  Joke.create!(category: joke[:category], joke: joke[:joke])
+end
